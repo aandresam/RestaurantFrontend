706f8385b5d03192a0db745bb003255d926e9d90
aandresam <aandres060916@gmail.com>
2026-01-09 01:36:13 -0500

    Actualizar la URL de la API en los entornos de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.gaido.solutions/api/v1',
+    apiUrl: 'https://restaurant-api.aguirrelabs.dev/api',
     appVersion: '1.0.0',
     featureFlags: {
         enableNewFeature: true
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://api.gaido.solutions/api/v1',
-    appVersion: '1.0.0',
-    featureFlags: {
-        enableNewFeature: true
-    }
+  production: true,
+  apiUrl: 'https://restaurant-api.aguirrelabs.dev/api',
+  appVersion: '1.0.0',
+  featureFlags: {
+    enableNewFeature: true,
+  },
 };
